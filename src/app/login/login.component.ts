76e5b1568fbe36ae784e93baa1dea86963c4db1a
Sylvia Engmann <d3vg0d355@theMambaMentality.local>
2021-03-19 10:03:33 +0000

    dashboard and navigation components

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from  '@angular/forms';
+import { Router } from  '@angular/router';
+import { User } from  '../user';
+import { AuthService } from  '../auth.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +12,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder) { 
+    loginForm: FormGroup;
+    //this.isSubmitted  =  false;
+  }
+
+
 
   ngOnInit(): void {
+  //   this.loginForm  =  this.formBuilder.group({
+  //     email: ['', Validators.required],
+  //     password: ['', Validators.required]
+  // });
   }
 
 }
